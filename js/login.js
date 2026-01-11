86f63a137dd695a5f17ef4d4a77665b9aeae144c
Giuseppe Granata <giuwalter4@icloud.com>
2026-01-11 21:44:51 +0100

    Aggiornamenti vari: aggiunta logica per la voce attiva nella sidebar, rimozione classi attive dai link, e integrazione di script globali nelle pagine HTML.

--- js/login.js
+++ js/login.js
@@ -6,7 +6,7 @@ function login() {
     
     let pass = document.getElementById("pass").value;
 
-    if (pass === "Ismael") {
+    if (pass === "Ismael1") {
         window.location.href = "dashboard.html";
     } else {
         document.getElementById("error").innerText = "Credenziali errate!";
